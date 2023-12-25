8d3dc5cb1e0a662343ad4bcecc3689c4c255ef7b
rahilwisdom <mister.r432000@gmail.com>
2023-12-26 03:03:18 +0700

    create template inbox components

---
+++ frontend/src/components/inbox.jsx
@@ -0,0 +1,17 @@
+// Inbox.jsx
+import React from 'react';
+import Navbar from '../components/navbar';
+
+const Inbox = () => {
+  // Logika untuk mendapatkan dan menampilkan daftar email masuk
+
+  return (
+    <div>
+      <Navbar />
+      <h2>Inbox</h2>
+      {/* Tampilkan daftar email masuk di sini */}
+    </div>
+  );
+};
+
+export default Inbox;
