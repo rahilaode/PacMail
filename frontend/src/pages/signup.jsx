9d4c93ec4e65dabd358204b024a1fd9976bcf930
rahilwisdom <mister.r432000@gmail.com>
2023-12-25 07:08:35 +0700

    update frontend stable 1

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -6,17 +6,34 @@ const SignUpForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLogin, setIsLogin] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState('');
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    setError('');
+
+    // Perform validation
+    if (!email || !password) {
+      setError('Please enter both email and password.');
+      return;
+    }
+
+    // Continue with login/signup logic
     if (isLogin) {
-      // Lakukan logika login di sini
       console.log('Login with:', email, password);
     } else {
-      // Lakukan logika signup di sini
-      // console.log('Sign Up:', fullName, email, password);
-      
+      console.log('Sign Up:', fullName, email, password);
     }
+
+    // Clear form after submission
+    setFullName('');
+    setEmail('');
+    setPassword('');
   };
 
   // Prefix pada judul halaman
@@ -25,7 +42,7 @@ const SignUpForm = () => {
   return (
     <div className="signup-container">
       <h2 style={{ color: '#fff' }}>{pageTitlePrefix} - PacMail</h2>
-      <form className="signup-form" onSubmit={handleFormSubmit}>
+      <form className="signup-form" onSubmit={handleFormSubmit} method="post">
         {!isLogin && (
           <div className="form-group">
             <label htmlFor="fullName">Full Name:</label>
@@ -46,15 +63,22 @@ const SignUpForm = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div className="form-group">
+        <div className="form-group password-input-container">
           <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div className="password-input">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <i
+              className={`eye-icon ${showPassword ? 'fas fa-eye' : 'fas fa-eye-slash'}`}
+              onClick={togglePasswordVisibility}
+            ></i>
+          </div>
         </div>
+        {error && <p className="error-message">{error}</p>}
         <button type="submit" className="submit-button">
           {isLogin ? 'Login' : 'Sign Up'}
         </button>
