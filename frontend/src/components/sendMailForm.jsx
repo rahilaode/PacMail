254b1626cb4853bfadc99fb780f6c8ed0739a3b1
rahilwisdom <mister.r432000@gmail.com>
2023-12-26 03:04:00 +0700

    create template sendmail components

---
+++ frontend/src/components/sendMailForm.jsx
@@ -0,0 +1,17 @@
+// SendEmail.jsx
+import React from 'react';
+import Navbar from '../components/navbar';
+
+const SendEmail = () => {
+  // Logika untuk menangani pengiriman email
+
+  return (
+    <div>
+      <Navbar />
+      <h2>Send Email</h2>
+      {/* Tampilkan formulir pengiriman email di sini */}
+    </div>
+  );
+};
+
+export default SendEmail;
