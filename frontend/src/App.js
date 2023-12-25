d5aab75ed2ccfbdd70ce404c11473d986185f747
rahilwisdom <mister.r432000@gmail.com>
2023-12-26 03:07:48 +0700

    add route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginSignup from "./pages/login-signup";
 import Mail from "./pages/mail";
+import SendEmail from './components/sendMailForm';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginSignup />} />
           <Route path="/mail/*" element={<Mail />} />
+          <Route path="/send-email" element={<SendEmail />} />
         </Routes>
       </BrowserRouter>
     </div>
